06d23ac8da50e66e7d522c741831222d39bfbfe3
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2022-06-09 18:11:05 +0200

    Add deploy script.

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+# clean
+rm -rf dist
+
+# build
+npm run build
+
+# navigate into the build output directory
+cd dist
+
+git init
+git checkout -b deploy
+git add -A
+git commit -m 'deploy'
+
+git push -f git@github.com:sirno/sirno.github.io.git deploy
+
+cd -
+
