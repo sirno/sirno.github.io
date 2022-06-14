7bbe64fe2c95d36016ded257edad4558e151864b
Nicolas Ochsner <nicolas.ochsner@env.ethz.ch>
2022-06-14 15:33:52 +0200

    Global link padding.

--- src/App.vue
+++ src/App.vue
@@ -101,13 +101,13 @@ nav a.router-link-exact-active:hover {
   background-color: transparent;
 }
 
-nav a {
+a {
   display: inline-block;
   padding: 0 1rem;
   border-left: 1px solid var(--color-border);
 }
 
-nav a:first-of-type {
+a:first-of-type {
   border: 0;
 }
 
