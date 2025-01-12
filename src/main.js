39da524fdaa459adae072430ed2352d470d12418
Nicolas Ochsner <nicolasochsner@gmail.com>
2025-01-12 19:10:51 +0100

    Add google analytics

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 
 import App from "./App.vue";
+import VueGtag from "vue-gtag";
 import router from "./router";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueGtag, {
+  config: { id: import.meta.env.GA_TAG_ID },
+});
 
 app.mount("#app");
